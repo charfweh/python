202506cf665c8a00f4481051e048dd876b1e4b73
Anuj Attri <anujattri01@gmail.com>
2020-07-16 16:52:36 +0530

    Added iter-prod.py

--- iter-prod.py
+++ iter-prod.py
@@ -2,10 +2,10 @@
 Given two lists, finding all possible combinations without repetition between the two lists.
 Useful for plotting values in a table (similar to a punnett square) & finding matrix multiplication.
 """
-import itertools as it
+import itertools
 
 list_1 = map(int, input("Input the first list:").split())
 list_2 = map(int, input("Input the second list:").split())
 
-product = list(it.product(list_1, list_2))
+product = list(itertools.product(list_1, list_2))
 print(f"Answer:{product}")
